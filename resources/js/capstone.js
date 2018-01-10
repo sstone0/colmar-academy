499843c81acbd31c6329c06b316fcb895ec12ce8
sstone18 <shawnrstone1995@gmail.com>
2018-01-10 16:17:22 -0500

    r - media/imgs/video a- json

--- resources/js/capstone.js
+++ resources/js/capstone.js
@@ -1,46 +1,56 @@
-$(document).ready(function() {
+//document ready function //
+$(function() {
 
-	//Cache reference to window and animation items (hidden)
-	var $hidden = $('.hidden');
-	var $window = $(window);
+	/////////Cache reference to window and animation items (hidden)/////////
+	const $hidden = $('.hidden');
+	const $window = $(window);
 
-	//measurements of window dimensions
-	function check_if_in_view() {
-		var window_height = $window.height();
-		var window_top_position = $window.scrollTop();
-		var window_bottom_position = (window_top_position + window_height);
+	if ($window.width() >= 750) {
 
-		$.each($hidden, function() {
-			var $element = $(this);
-			var element_height = $element.outerHeight();
-			var element_top_position = $element.offset().top;
-			var element_bottom_position = (element_top_position + element_height);
+		///////measurements of window dimensions///////
+		function check_if_in_view() {
+			let window_height = $window.height();
+			let window_top_position = $window.scrollTop();
+			let window_bottom_position = (window_top_position + window_height);
 
-			//check to see if this current container is within viewport
-			if ((element_bottom_position >= window_top_position) &&
-				(element_top_position <= window_bottom_position)) {
-				$element.addClass('bounceUp');
+			$.each($hidden, function() {
+				let $element = $(this);
+				let element_height = $element.outerHeight();
+				let element_top_position = $element.offset().top;
+				let element_bottom_position = (element_top_position + element_height);
+
+				////////check to see if this current container is within viewport////////
+				if ((element_bottom_position >= window_top_position) &&
+					(element_top_position <= (window_bottom_position - 112))) {
+					$element.addClass('bounceUp');
+				}
+			});
+		};
+
+		$window.on('scroll resize', check_if_in_view);
+		$window.trigger('scroll');
+
+
+		//////////changing color of header and nav//////////
+		$window.on('scroll', function changeHeaderColor() {
+			let top = $('#top');
+			let headSpan = $('#headSpan');
+			let a = $('a')
+			let scrollTop = $window.scrollTop()
+
+			if (scrollTop >= 120) {
+				top.css({'background-color': '#4A4A4A', 'color': 'white'});
+				headSpan.css({'color' : 'white'});
+				a.css({'color' : 'white'});
+			} else {
+				top.css({'background-color': 'white', 'color': 'black'});
+				headSpan.css({'color' : 'black'});
+				a.css({'color' : '#4A4A4A'});
 			}
 		});
+
+	} else {
+		$(".hidden").removeClass("hidden");
 	}
-	$window.on('scroll resize', check_if_in_view);
-	$window.trigger('scroll');
-
-	//removes hidden class for mobile screens
-	function noFade() {
-		let windowWidth = document.body.clientWidth;
-		let banner = document.getElementById('banner').classList;
-		let information = document.getElementById('information').classList;
-		let learning = document.getElementById('learning').classList;
-		let thesis = document.getElementById('thesis').classList;
-
-		if (windowWidth <= 750) {
-			banner.remove("hidden");
-			information.remove("hidden");
-			learning.remove("hidden");
-			thesis.remove("hidden");
-		}
-	};
-	noFade();
 
 });
